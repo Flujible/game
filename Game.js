39073c9e59448f979119d819eaa8a4940f1fadce
George Bryant <george.s.bryant@gmail.com>
2020-08-02 23:26:10 +0100

    Move game control elements into Game class

--- Game.js
+++ Game.js
@@ -1,9 +1,29 @@
 import { Player } from "./Player.js";
 
 export default class Game {
-    player;
-    
-    constructor() {
+    players = {};
+    possibleColours = ['pink', 'orange', 'blue'];
+    takenColours = [];
 
-    }    
+    constructor() {}    
+
+    get players() {
+        return this.players;
+    }
+
+    set players(newPlayers) {
+        this.players = newPlayers;
+    }
+
+    createPlayer(id) {
+        const colour = this.possibleColours.find(colour => !this.takenColours.includes(colour));
+        this.takenColours.push(colour);
+        this.players[id] = new Player(id, colour);
+    }
+
+    removePlayer(id) {
+        const availableColour = this.players[id].colour;
+        delete this.players[id];
+        this.takenColours.splice(this.takenColours.findIndex(colour => availableColour === colour), 1)
+    }
 }
--- app.js
+++ app.js
@@ -2,16 +2,20 @@ import express from 'express'
 import http from 'http';
 import path from 'path';
 import socketIO from 'socket.io';
+import Game from './Game.js';
+
 const __dirname = path.resolve();
 
-let  players = [];
+const game = new Game();
+
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server, {
     allowRequest: (req, fn) => {
-        players.length < 3 ? fn(200, true) : fn("Server full", false);
+        Object.keys(game.players).length < 3 ? fn(200, true) : fn("Server full", false);
     }
 });
+
 
 app.set('port', 5000);
 app.use(express.static(path.join(__dirname, '/static')));
@@ -25,13 +29,18 @@ server.listen(5000, () => {
 });
 
 io.on('connection', socket => {
-    players.push(socket.id);
-    socket.emit('id', socket.id);
-    socket.broadcast.emit('info', players);
+    game.createPlayer(socket.id);
+    socket.emit('id', game.players[socket.id]);
+    socket.broadcast.emit('info', game.players);
 
     socket.on('disconnect', reason => {
-        players = players.filter(id => id !== socket.id)
+        game.removePlayer(socket.id);
         console.log("A player has disconnected")
         socket.broadcast.emit('info', `${reason} :: ${socket.id}`);
-    })
+    });
+
+    socket.on('move', movement => {
+        game.players[socket.id].move(movement);
+        io.sockets.emit('playerLocations', game.players)
+    });
 });
